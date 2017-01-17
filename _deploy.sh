ae02c3e0a03e8ecc20a46c5fc3f95bc57d5a69aa
Maximilian Held <info@maxheld.de>
2017-01-17 22:52:14 +0100

    deploy via source to master, also fix typo

--- _deploy.sh
+++ _deploy.sh
@@ -3,15 +3,15 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "source" ] && exit 0
 
 git config --global user.email "info@maxheld.de"
 git config --global user.name "Maximilian Held"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git blogdown-output
+git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git blogdown-output
 cd blogdown-output
 git rm -rf .
 cp -r ../public/* ./
 git add --all *
 git commit -m "Update webpage" || true
-git push origin gh-pages
+git push origin master
